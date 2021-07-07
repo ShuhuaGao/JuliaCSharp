d4c867d8ac2751d680301f446b890e66093a8c64
Shuhua <nus.gao.shuhua@gmail.com>
2021-07-07 21:10:26 +0800

    console project setup

---
+++ CSharpClient/ConsoleClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NetMQ.Sockets;
+using MessagePack;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // solve linear equation Ax = b
+
+        }
+    }
+}
