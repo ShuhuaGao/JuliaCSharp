5351c456d5477c44ee768a50b8f28ee6ba8b62c3
Shuhua <nus.gao.shuhua@gmail.com>
2021-07-12 22:15:10 +0800

    embed setup

---
+++ Embedding/EJStarter/Julia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace EJStarter
+{
+    class Julia
+    {
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void jl_init__threading();
+    }
+}
---
+++ Embedding/EJStarter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EJStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Julia.jl_init__threading();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
