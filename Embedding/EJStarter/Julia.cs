f1650981ceb6b296f79629488ebc66f84e16d5b6
Shuhua <nus.gao.shuhua@gmail.com>
2021-07-13 22:11:04 +0800

    call sin in a verbose way

--- Embedding/EJStarter/Julia.cs
+++ Embedding/EJStarter/Julia.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿// Main documentation:  https://docs.julialang.org/en/v1/manual/embedding/#Embedding-Julia
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.InteropServices;
 
 namespace EJStarter
@@ -25,5 +25,20 @@ namespace EJStarter
 
         [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr jl_get_global(IntPtr module, IntPtr sym);
+
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr jl_call1(IntPtr function, IntPtr arg);
+
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr jl_box_float64(double x);
+
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double jl_unbox_float64(IntPtr v);
+
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr jl_exception_occurred();
+
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr jl_typeof_str(IntPtr value);
     }
 }
--- Embedding/EJStarter/Program.cs
+++ Embedding/EJStarter/Program.cs
@@ -6,12 +6,30 @@ namespace EJStarter
     {
         static void Main(string[] args)
         {
-            Julia.jl_init__threading(); // correct
-            Julia.jl_eval_string("println(sqrt(3.14))");
+            Julia.jl_init__threading();
 
+
+            // Julia default IO output is redirected here automatically
+            Julia.jl_eval_string("println(sin(2.34))");
+
+
+            // get sin(2.34): the concise way
+            // jl_eval_string returns a jl_value_t*, which is a pointer to a heap-allocated Julia object.
+            IntPtr r = Julia.jl_eval_string("sin(2.34)");
+            Console.WriteLine($"r_unboxed = {Julia.jl_unbox_float64(r)}");
+
+
+            // get sin(2.34): the verbose but more general way
             IntPtr mBase = Julia.jl_eval_string("Base");
             IntPtr symSin = Julia.jl_symbol("sin");
-            IntPtr sin = Julia.jl_get_global(mBase, symSin);
+            IntPtr sin = Julia.jl_get_global(mBase, symSin); // get `Base.sin`
+            IntPtr ret = Julia.jl_call1(sin, Julia.jl_box_float64(2.34));
+            // not memory safe here: see https://docs.julialang.org/en/v1/manual/embedding/#Memory-Management
+            double ret_unboxed = Julia.jl_unbox_float64(ret);
+            Console.WriteLine($"ret_unboxed = {ret_unboxed}");
+
+
+
 
             Julia.jl_atexit_hook(0);
             Console.WriteLine("Hello World!");
