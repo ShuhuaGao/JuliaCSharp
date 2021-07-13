09753c4d2e7a6b34b2a914cbf27c169a319180de
Shuhua <nus.gao.shuhua@gmail.com>
2021-07-13 21:09:15 +0800

    call function 1

--- Embedding/EJStarter/Julia.cs
+++ Embedding/EJStarter/Julia.cs
@@ -6,9 +6,24 @@ using System.Runtime.InteropServices;
 
 namespace EJStarter
 {
+    // see https://github1s.com/JuliaLang/julia/blob/HEAD/src/julia.h
     class Julia
     {
+        // normally, "libjulia.dll" can be found automatically
+        // check https://stackoverflow.com/questions/8836093/how-can-i-specify-a-dllimport-path-at-runtime/8861895
         [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern void jl_init__threading();
+
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr jl_eval_string(string str);
+
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void jl_atexit_hook(int status);
+
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr jl_symbol(string str);
+
+        [DllImport("libjulia.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr jl_get_global(IntPtr module, IntPtr sym);
     }
 }
--- Embedding/EJStarter/Program.cs
+++ Embedding/EJStarter/Program.cs
@@ -6,7 +6,14 @@ namespace EJStarter
     {
         static void Main(string[] args)
         {
-            Julia.jl_init__threading();
+            Julia.jl_init__threading(); // correct
+            Julia.jl_eval_string("println(sqrt(3.14))");
+
+            IntPtr mBase = Julia.jl_eval_string("Base");
+            IntPtr symSin = Julia.jl_symbol("sin");
+            IntPtr sin = Julia.jl_get_global(mBase, symSin);
+
+            Julia.jl_atexit_hook(0);
             Console.WriteLine("Hello World!");
         }
     }
