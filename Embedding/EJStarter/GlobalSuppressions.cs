76be95b574599b3dd1264f263d5089a3098b4081
Gao Shuhua <shuhua_gao@u.nus.edu>
2021-07-14 11:27:30 +0800

    suppress marshal analyzer warning

---
+++ Embedding/EJStarter/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Globalization", "CA2101:Specify marshaling for P/Invoke string arguments", Justification = "<Pending>", Scope = "member", Target = "~M:EJStarter.Julia.jl_symbol(System.String)~System.IntPtr")]
+[assembly: SuppressMessage("Globalization", "CA2101:Specify marshaling for P/Invoke string arguments", Justification = "<Pending>", Scope = "member", Target = "~M:EJStarter.Julia.jl_eval_string(System.String)~System.IntPtr")]
